5656ec38d05204d652ca5cc786feb9754b73b4d4
Shyam Patel <shyam.p@ahduni.edu.in>
2019-11-19 11:24:26 +0530

    Arduino Code for MQ135 Gas Sensor

---
+++ Arduino_co2.ino
@@ -0,0 +1,43 @@
+// The load resistance on the board
+//#define RLOAD 22.0
+// Calibration resistance at atmospheric CO2 level
+//#define RZERO 879.13
+
+//#define RZERO 76.63
+//
+//#include "MQ135.h"
+//
+//const int ANALOGPIN = 2;
+//MQ135 gasSensor = MQ135(ANALOGPIN);
+//
+//
+//void setup() {
+//  Serial.begin(9600);
+//}
+// 
+//void loop() {
+//
+//  float zero = gasSensor.getRZero();
+//  Serial.print ("rzero: ");
+//  Serial.println (zero);
+//  float ppm = gasSensor.getPPM();
+//  Serial.print ("ppm: ");
+//  Serial.println (ppm);
+//  delay(1000);
+//} 
+
+#include "MQ135.h"
+
+void setup (){
+Serial.begin (115200);
+}
+void loop() {
+MQ135 gasSensor = MQ135(A2); // Attach sensor to pin A0
+String rzero = (String)gasSensor.getRZero();
+if((Serial.available())>0){
+  Serial.println (rzero);  
+}
+
+
+delay(5000);
+}
